d8fa3569ca00e21895ce62dcff2312ec78d3be5a
jgrimes91 <jessica.grimes@my.jcu.edu.au>
2020-08-17 19:13:55 +1000

    Loop now works properly

--- week4/clean_calculator.py
+++ week4/clean_calculator.py
@@ -4,25 +4,33 @@ MENU = "1. Add\n2. Subtract\n3. Multiply\n4. Divide\n5. Quit"
 def main():
     print("Welcome to the Clean Calculator!")
     loop_controller = 0
-    result = 0
+    num_one = 0
+    num_two = 0
     while loop_controller == 0:
         print(MENU)
         choice = get_valid_selection(">>> ")
 
-        num_one = get_valid_number("Enter the first number: ")
-        num_two = get_valid_number("Enter the second number: ")
-
         if choice == 5:
             print("Goodbye!")
+            loop_controller = 1
             quit()
-        elif choice == 1:
+        else:
+            num_one = get_valid_number("Enter the first number: ")
+            num_two = get_valid_number("Enter the second number: ")
+
+        if choice == 1:
+            print("Addition")
             result = addition(num_one, num_two)
         elif choice == 2:
+            print("Subtraction")
             result = subtract(num_one, num_two)
         elif choice == 3:
+            print("Multiplication")
             result = multiply(num_one, num_two)
         else:
+            print("Division")
             result = divide(num_one, num_two)
+
         print("The result is {}".format(result))
 
 
@@ -32,7 +40,7 @@ def get_valid_selection(prompt):
         user_input = input(prompt)
         try:
             user_input = int(user_input)
-            if user_input <= 0 or user_input >= 5:
+            if user_input <= 0 or user_input >= 6:
                 print("Choice must between between 1 and 5.")
                 print(MENU)
             else:
